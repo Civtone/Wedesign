551bf15605ce87813e8d2a850f4aa023e13b4bb0
AdithiRanganatha <Adithiranganatha@fico.com>
2025-04-22 11:51:37 +0530

    Initial commit for Civtone website

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-black text-white text-center py-4">
+    <p>Contact us at: civtonedesign@gmail.com | +9972023325</p>
+  </footer>
+);
+
+export default Footer;
